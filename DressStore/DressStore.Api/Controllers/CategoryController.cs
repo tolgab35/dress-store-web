63534cb4c31452694a0881614ab61ad41f7fce49
Sema Gül Çelik <segece3308@gmail.com>
2025-08-14 23:48:42 +0300

    Bazı modeller ve modellere ait denetleyiciler eklendi.

---
+++ DressStore/DressStore.Api/Controllers/CategoryController.cs
@@ -0,0 +1,17 @@
+﻿using dress_store_web.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace dress_store_web.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class CategoryController : ControllerBase
+    {
+        private readonly Category category;
+
+        public CategoryController(Category category)
+        {
+            this.category = category;
+        }
+    }
+}
