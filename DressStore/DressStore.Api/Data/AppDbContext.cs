897b16ba7345cd9e812cabf38bedeeb4816484a8
Tolga <tolgaboz350@gmail.com>
2025-08-16 00:38:21 +0300

    refactor: update DbSet properties to use expression-bodied syntax with Set<T>()

--- DressStore/DressStore.Api/Data/AppDbContext.cs
+++ DressStore/DressStore.Api/Data/AppDbContext.cs
@@ -3,20 +3,21 @@ using dress_store_web.Models;
 
 namespace DressStore.Api.Data
 {
-    public class AppDbContext  : DbContext
+    public class AppDbContext : DbContext
     {
-        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
-        {
-        }
-        public DbSet<Category> Categories { get; set; }
-        public DbSet<Product> Products { get; set; }
-        public DbSet<ProductImage> ProductImages { get; set; }
-        public DbSet<ProductVariant> ProductVariants { get; set; }
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options) { }
+
+        public DbSet<Category> Categories => Set<Category>();
+        public DbSet<Product> Products => Set<Product>();
+        public DbSet<ProductImage> ProductImages => Set<ProductImage>();
+        public DbSet<ProductVariant> ProductVariants => Set<ProductVariant>();
+
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
             base.OnModelCreating(modelBuilder);
-            // Additional model configuration can go here
+
+            // Tüm IEntityTypeConfiguration<> sınıflarını otomatik uygula
+            modelBuilder.ApplyConfigurationsFromAssembly(typeof(AppDbContext).Assembly);
         }
     }
-    }
-
+}
