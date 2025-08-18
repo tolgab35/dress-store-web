f2d280f43bc33db168c6729db4f686f1b4eae1a2
Sema Gül Çelik <segece3308@gmail.com>
2025-08-19 00:13:24 +0300

    Validation, Response ve kaynaklar sınıfı eklendi.

---
+++ DressStore/DressStore.Api/Response.cs
@@ -0,0 +1,9 @@
+﻿namespace DressStore.Api
+{
+    public class Response<T>
+    {
+        public T data { get; set; }
+        public string message { get; set; }
+        public bool success { get; set; }
+    }
+}
---
+++ DressStore/DressStore.Api/Validation.cs
@@ -0,0 +1,28 @@
+﻿using DressStore.Api.Resources;
+
+namespace DressStore.Api
+{
+    public class Validation
+    {
+        public static bool IsValidName(string str, out string message)
+        {
+            if (string.IsNullOrWhiteSpace(str))
+            {
+                message = Resource.NameRequired;
+                return false;
+            }
+            if (str.ToLower().StartsWith("ğ"))
+            {
+                message = Resource.NameCannotStartsWithGh; 
+                return false;
+            }
+            if (!char.IsUpper(str[0]))
+            {
+                message = Resource.NameMustStartWithUppercase;
+                return false;
+            }
+            message = "";
+            return true;
+        }
+    }
+}
